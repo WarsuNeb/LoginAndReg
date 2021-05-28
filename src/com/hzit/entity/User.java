1b60921eac2bdbc629ba18acaf80ee2526705c8e
WarsuNeb <ge1967116tkr@126.com>
2021-05-28 10:02:37 +0800

    fdgh

--- src/com/hzit/entity/User.java
+++ src/com/hzit/entity/User.java
@@ -1,5 +1,59 @@
 package com.hzit.entity;
 
 public class User {
-	
-}
+	private int id;//用户id
+	private String name;//用户姓名
+	private String password;//用户
+	private int age;//用户姓名
+
+	public User() {
+		super();
+	}
+
+	public User(int id, String name, String password, int age) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.password = password;
+		this.age = age;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", password=" + password
+				+ ", age=" + age + "]";
+	}
+
+	}
